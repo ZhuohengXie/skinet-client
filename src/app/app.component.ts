9338473bbc058a471e7c641dd7b2218f091c7a94
Zhuoheng Xie <zh-xie@live.com>
2020-10-27 14:30:04 +0800

    update client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Skinet'; // used as {{title}} in app.component.html
+  products: any[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: any) => {
+      // console.log(response);
+      this.products = response; // reponse.data?
+      // console.log(this.products);
+    }, error => {
+      console.log(error);
+    });
+  }
 }
