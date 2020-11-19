236717261184fc373d53cdd476c1b156d4400bba
Zhuoheng Xie <zh-xie@live.com>
2020-11-19 16:03:17 +0800

    End of Section 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,15 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './shared/models/pagination';
-import { IProduct } from './shared/models/product';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Skinet'; // used as {{title}} in app.component.html
-  products: IProduct[];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
-      (response: IPagination) => {
-      // console.log(response);
-      this.products = response.data; // reponse.data?
-      // console.log(this.products);
-    }, error => {
-      console.log(error);
-    });
   }
 }
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 
 @NgModule({
-  declarations: [ShopComponent],
+  declarations: [ShopComponent, ProductDetailsComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [ShopComponent]
 })
 export class ShopModule { }
